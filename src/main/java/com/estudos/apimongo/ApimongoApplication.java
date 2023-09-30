984101dfc9a67006f5f289f42c559fa908ef5f8a
Gustavo Muzell <gustavomuzell1234@gmail.com>
2023-09-30 15:56:09 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/estudos/apimongo/ApimongoApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.apimongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/apimongo/ApimongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.apimongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
